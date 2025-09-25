feb8c5b519d8b7bdee1606570ed50dbe676a7257
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-09-25 13:15:32 -0300

    Create score service tests

--- src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
@@ -1,21 +1,88 @@
 package com.devsuperior.dsmovie.services;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.entities.ScoreEntity;
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import com.devsuperior.dsmovie.tests.ScoreFactory;
+import com.devsuperior.dsmovie.tests.UserFactory;
 
 @ExtendWith(SpringExtension.class)
 public class ScoreServiceTests {
-	
+
 	@InjectMocks
-	private ScoreService service;
-	
-	@Test
-	public void saveScoreShouldReturnMovieDTO() {
+	private ScoreService scoreService;
+
+	@Mock
+	private UserService userService;
+
+	@Mock
+	private MovieRepository movieRepository;
+
+	@Mock
+	private ScoreRepository scoreRepository;
+
+	private Long existingMovieId;
+	private Long nonExistingMovieId;
+	private UserEntity user;
+	private MovieEntity movie;
+	private ScoreEntity score;
+
+	@BeforeEach
+	void setUp() throws Exception {
+
+		existingMovieId = 1L;
+		nonExistingMovieId = 1000L;
+
+		user = UserFactory.createUserEntity();
+		movie = MovieFactory.createMovieEntity();
+		score = ScoreFactory.createScoreEntity();
+		// for test sum scores
+		movie.getScores().add(score);
+
+		Mockito.when(userService.authenticated()).thenReturn(user);
+		Mockito.when(movieRepository.findById(existingMovieId)).thenReturn(Optional.of(movie));
+		Mockito.when(movieRepository.findById(nonExistingMovieId)).thenReturn(Optional.empty());
+		Mockito.when(scoreRepository.saveAndFlush(any())).thenReturn(score);
+		Mockito.when(movieRepository.save(any())).thenReturn(movie);
+
 	}
-	
+
 	@Test
-	public void saveScoreShouldThrowResourceNotFoundExceptionWhenNonExistingMovieId() {
+	public void saveScore_ShouldReturnMovieDTO_WhenValidData() {
+
+		MovieDTO result = scoreService.saveScore(new ScoreDTO(score));
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getTitle(), movie.getTitle());
+		Assertions.assertEquals(result.getScore(), movie.getScore());
+
 	}
+
+	@Test
+	public void saveScore_ShouldThrowResourceNotFoundException_WhenNonExistingMovieId() {
+
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			scoreService.saveScore(new ScoreDTO(nonExistingMovieId, 5.0));
+		});
+	}
+
 }
--- src/test/java/com/devsuperior/dsmovie/tests/ScoreFactory.java
+++ src/test/java/com/devsuperior/dsmovie/tests/ScoreFactory.java
@@ -6,23 +6,23 @@ import com.devsuperior.dsmovie.entities.ScoreEntity;
 import com.devsuperior.dsmovie.entities.UserEntity;
 
 public class ScoreFactory {
-	
+
 	public static Double scoreValue = 4.5;
-	
+
 	public static ScoreEntity createScoreEntity() {
 		MovieEntity movie = MovieFactory.createMovieEntity();
 		UserEntity user = UserFactory.createUserEntity();
 		ScoreEntity score = new ScoreEntity();
-		
+
 		score.setMovie(movie);
 		score.setUser(user);
 		score.setValue(scoreValue);
 		movie.getScores().add(score);
 		return score;
 	}
-	
+
 	public static ScoreDTO createScoreDTO() {
 		ScoreEntity score = createScoreEntity();
-		return new ScoreDTO(score.getId().getMovie().getId(), score.getValue());
+		return new ScoreDTO(score);
 	}
 }
